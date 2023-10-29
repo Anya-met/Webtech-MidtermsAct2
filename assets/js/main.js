f1259a218b8ac11d70953a16b3e892082af28336
Jeeian <2223900@slu.edu.ph>
2023-10-29 15:54:21 +0800

    SUBSCRIBE BUTTON MODIFIED&FIXED

--- assets/js/main.js
+++ assets/js/main.js
@@ -172,3 +172,13 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+//SUBSCRIBE
+const form = document.querySelector("form");
+const card_1 = document.querySelector(".card");
+const card_2 = document.querySelector(".card-2");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  card_1.classList.add("hide");
+  card_2.classList.remove("hide");
+});
